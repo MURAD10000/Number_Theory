e961501b7ef58431ae16e60c0d95d005b579dca4
MURAD10000 <74201005+MURAD10000@users.noreply.github.com>
2020-12-11 05:15:22 +0600

    Add files via upload

---
+++ NcR_Mod_by_bottom_up.cpp
@@ -0,0 +1,12 @@
+
+//NcR mod by boottom up
+
+for(int i=0;i<=n;i++)
+{
+        for(int j=0;j<=i;j++)
+		{
+            if(j==0 || j==i) nCr[i][j]=1;
+            else nCr[i][j]=(nCr[i-1][j-1]+nCr[i-1][j])%M;
+        }
+}
+
