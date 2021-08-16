ca1287bfec9422f259026c668b1840d2410cdc85
MURAD10000 <74201005+MURAD10000@users.noreply.github.com>
2021-08-16 14:52:01 +0600

    Update NcR_Mod_by_bottom_up.cpp

--- NcR_Mod_by_bottom_up.cpp
+++ NcR_Mod_by_bottom_up.cpp
@@ -1,10 +1,11 @@
 
 //NcR mod by boottom up
-
+const int M=1e9+7 ;
+ll nCr[1005][1005];
 for(int i=0;i<=n;i++)
 {
         for(int j=0;j<=i;j++)
-		{
+	{
             if(j==0 || j==i) nCr[i][j]=1;
             else nCr[i][j]=(nCr[i-1][j-1]+nCr[i-1][j])%M;
         }
